83f778924dc1cb070ae373b5c3586eee1f26b170
Neo Lee <neo.lee@gmail.com>
2025-04-30 21:55:00 +0800

    feat: add embedder test

---
+++ embedders.py
@@ -0,0 +1,21 @@
+from mal.providers import provider_by_alias
+from mal.openai.embedder import Embedder
+
+
+_local_provider = provider_by_alias("local")
+
+nomic = Embedder(_local_provider, "nomic", 768)
+snowflake = Embedder(_local_provider, "snowflake", 1024)
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    async def test_embedder(s: str, embedder: Embedder):
+        embedding = await embedder.create_embedding(s)
+        print(f"Embeddings: {embedding[:5]}")
+        print(f"Dimensions: {len(embedding)}")
+
+    s = "The quick brown fox jumps over the lazy dog."
+    asyncio.run(test_embedder(s, nomic))
+    asyncio.run(test_embedder(s, snowflake))
