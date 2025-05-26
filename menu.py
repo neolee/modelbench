5d3cee9f24d8b1b59e380e4ece138b3552ab21b3
Neo Lee <neo.lee@gmail.com>
2025-05-26 19:17:14 +0800

    fix: update mal and better display on menu

--- menu.py
+++ menu.py
@@ -4,6 +4,6 @@ from simple_term_menu import TerminalMenu
 
 # show menu of a collection, whose element should have a `description` attribute
 def show_menu_of(collection: Iterable, title: str) -> int | Tuple[int, ...] | None:
-    items = [f"[{idx}] {obj.description}" for idx, obj in enumerate(collection)]
+    items = [f"[{idx:02d}] {obj.description}" for idx, obj in enumerate(collection)]
     terminal_menu = TerminalMenu(items, title=title)
     return terminal_menu.show()
