a4b5809e2e169166b2f9cae73e76e095f5a83a24
Neo Lee <neo.lee@gmail.com>
2025-07-16 17:34:06 +0800

    env: add kimi k2 model

--- models.py
+++ models.py
@@ -9,6 +9,9 @@ qwen = model_by_provider(mal.qwen_provider, description="Qwen2.5-Max")
 qwen_coder = model_by_provider(mal.qwen_provider, "coder", "Qwen2.5-Coder-Plus")
 qwen_reasoner = model_by_provider(mal.qwen_provider, "reasoner", "QwQ-Plus")
 
+kimi_k2 = model_by_provider(mal.moonshot_provider, description="Kimi K2")
+kimi_reasoner = model_by_provider(mal.moonshot_provider, "reasoner", "Kimi Thinking")
+
 openrouter = model_by_provider(mal.openrouter_provider, description="Gemini 2.5 Flash Preview")
 openrouter_gemini_flash = model_by_provider_with_model(mal.openrouter_provider, "google/gemini-2.5-flash-preview-05-20", "Gemini 2.5 Flash Preview")
 openrouter_gemini_flash_thinking = model_by_provider_with_model(mal.openrouter_provider, "google/gemini-2.5-flash-preview-05-20:thinking", "Gemini 2.5 Flash Preview (thinking)")
@@ -26,8 +29,8 @@ lmstudio = model_by_provider(mal.lmstudio_provider, description="LM Studio Defau
 
 ollama = model_by_provider(mal.ollama_provider, description="Ollama Default")
 
-models = [deepseek, deepseek_reasoner, qwen, qwen_coder, qwen_reasoner,
-          openrouter_gemini_flash, openrouter_gemini_pro, openrouter_grok, openrouter_k2,
+models = [deepseek, deepseek_reasoner, qwen, qwen_coder, qwen_reasoner, kimi_k2,
+          openrouter_gemini_flash, openrouter_gemini_pro, openrouter_grok,
           local_qwen, local_qwen_nothink, local_gemma, local_devstral, lmstudio]
 
 default = deepseek
