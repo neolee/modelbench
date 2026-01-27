7d0fd3ae0adf079e9c259b02bba7d5788b584746
Neo Lee <neo.lee@gmail.com>
2026-01-27 15:22:21 +0800

    env: update model definition

--- models.py
+++ models.py
@@ -5,11 +5,11 @@ deepseek = Model("deepseek/deepseek-chat", name="DeepSeek-V3.2 (Non-thinking)")
 deepseek_reasoner = Model("deepseek/deepseek-reasoner", name="DeepSeek-V3.2")
 
 qwen = Model("qwen/qwen-plus-latest", name="Qwen Plus")
-qwen_max = Model("qwen/qwen3-max-preview", name="Qwen Max")
+qwen_reasoner = Model("qwen/qwen3-max-2026-01-23", name="Qwen Max")
 qwen_coder = Model("qwen/qwen3-coder-plus", name="Qwen-Coder Plus")
 
 kimi = Model("moonshot/kimi-k2-turbo-preview", name="Kimi K2")
-kimi_reasoner = Model("moonshot/kimi-k2-thinking", name="Kimi K2 Thinking")
+kimi_reasoner = Model("moonshot/kimi-k2.5", name="Kimi K2.5")
 
 glm = Model("zhipu/glm-4.7", name="GLM-4.7")
 
@@ -20,7 +20,7 @@ openrouter_grok = Model("openrouter/x-ai/grok-4.1-fast", name="Grok 4.1 Fast")
 local = Model("local/qwen3", name="Qwen3-30B-A3B 2507")
 local_reasoner = Model("local/qwen3-thinking", name="Qwen3-30B-A3B 2507 (Thinking)")
 
-models = [deepseek, deepseek_reasoner, qwen, qwen_max, qwen_coder, kimi, kimi_reasoner, glm, 
+models = [deepseek, deepseek_reasoner, qwen, qwen_reasoner, qwen_coder, kimi, kimi_reasoner, glm, 
           openrouter_gemini_flash, openrouter_gemini_pro, openrouter_grok,
           local, local_reasoner]
 
