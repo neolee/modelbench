615896a763ba06ad305dcf32e33316a885e764f4
Neo Lee <neo.lee@gmail.com>
2026-04-24 17:14:42 +0800

    env: update mal and all packages

--- models.py
+++ models.py
@@ -1,17 +1,17 @@
 from mal.adapter.openai import Model
 
 
-deepseek = Model("deepseek/deepseek-chat", name="DeepSeek-V3.2 (Non-thinking)")
-deepseek_reasoner = Model("deepseek/deepseek-reasoner", name="DeepSeek-V3.2")
+deepseek_flash = Model("deepseek/deepseek-v4-flash", name="DeepSeek-V4-Flash")
+deepseek_pro = Model("deepseek/deepseek-v4-pro", name="DeepSeek-V4-Pro")
 
 qwen = Model("qwen/qwen3.6-plus", name="Qwen3.6 Plus")
 
 kimi = Model("moonshot/kimi-k2-turbo-preview", name="Kimi K2")
-kimi_reasoner = Model("moonshot/kimi-k2.5", name="Kimi K2.5")
+kimi_reasoner = Model("moonshot/kimi-k2.6", name="Kimi K2.6")
 
 glm = Model("zhipu/glm-5.1", name="GLM-5.1")
 
-gpt = Model("openai", name="GPT-5.4")
+gpt = Model("openai", name="GPT-5.5")
 
 openrouter_gemini_flash = Model("openrouter/google/gemini-3-flash-preview", name="Gemini 3 Flash")
 openrouter_gemini_pro = Model("openrouter/google/gemini-3.1-pro-preview", name="Gemini 3.1 Pro")
@@ -20,11 +20,10 @@ openrouter_grok = Model("openrouter/x-ai/grok-4.1-fast", name="Grok 4.1 Fast")
 local = Model("local/qwen3.6-nt", name="Qwen3.6-35B-A3B Non-thinking")
 local_reasoner = Model("local/qwen3.6", name="Qwen3.6-35B-A3B")
 
-omlx_qwen_35b = Model("omlx", "Qwen3.6-35B-A3B")
-omlx_gemma_26b = Model("omlx/gemma-4-26b", "Gemma-4-26B-A4B")
+omlx_qwen_35b = Model("omlx", "Qwen3.6-35B-A3B MLX")
 
-models = [deepseek, deepseek_reasoner, qwen, kimi, kimi_reasoner, glm, 
+models = [deepseek_flash, deepseek_pro, qwen, kimi, kimi_reasoner, glm, 
           gpt, openrouter_gemini_flash, openrouter_gemini_pro, openrouter_grok,
-          local, local_reasoner, omlx_qwen_35b, omlx_gemma_26b]
+          local, local_reasoner, omlx_qwen_35b]
 
-default = deepseek
+default = deepseek_flash
